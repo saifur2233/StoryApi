fa3a7bcd79227e86653c4b7ff305d3a1f3bdf07a
saifur2233 <saifurnstuiit223344@gmail.com>
2022-08-29 14:13:55 +0600

    db connection added

--- src/main/java/com/example/storyapi/StoryApiApplication.java
+++ src/main/java/com/example/storyapi/StoryApiApplication.java
@@ -3,10 +3,11 @@ package com.example.storyapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class StoryApiApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(StoryApiApplication.class, args);
     }
 
