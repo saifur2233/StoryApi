3e5b38f043bef3f663653839dc058e03cadf96c8
saifur2233 <saifurnstuiit223344@gmail.com>
2022-08-29 19:26:44 +0600

    issue-2 password and email validation

--- src/main/java/com/example/storyapi/models/Users.java
+++ src/main/java/com/example/storyapi/models/Users.java
@@ -1,16 +1,19 @@
 package com.example.storyapi.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
 @Entity
 public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name="email", unique=true)
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
     private String  email;
+    @Pattern(regexp ="^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&]{8,}$")
     private String password;
 
     public Users(){
