6345b3c3592b08b9a8c0db8c766e6d3872ca7fa4
saifur2233 <saifurnstuiit223344@gmail.com>
2022-08-29 09:24:14 +0600

    first commit

---
+++ src/main/java/com/example/storyapi/StoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storyapi/StoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
