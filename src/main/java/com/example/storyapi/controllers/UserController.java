1fe2d29ba3f4f107ef5b695d316e2a0b64f33c47
saifur2233 <saifurnstuiit223344@gmail.com>
2022-08-30 09:16:50 +0600

    unecessary comments successfully removed

--- src/main/java/com/example/storyapi/controllers/UserController.java
+++ src/main/java/com/example/storyapi/controllers/UserController.java
@@ -13,32 +13,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // Get all users
     @GetMapping(value = "/users")
     public List<Users> getAllUsers(){
         return userService.getAllUsers();
     }
 
 
-    // Create user
     @RequestMapping(method = RequestMethod.POST, value = "/users")
     public void addUser(@RequestBody Users users){
         userService.addUser(users);
     }
 
-     //Get a user by id
     @GetMapping(value = "/users/{id}")
     public Optional<Users> getUser(@PathVariable int id) {
         return userService.getUser(id);
     }
 
-    // User info update
     @PutMapping(value = "/users/{email}")
     public String updateUser(@PathVariable String email, @RequestBody Users users){
          return userService.updateUser(email, users);
     }
 
-    // user delete
     @RequestMapping(method = RequestMethod.DELETE, value = "/users")
     public void deleteUser(@RequestBody Users users){
         userService.deleteUser(users);
