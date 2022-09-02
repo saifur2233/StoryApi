61effc87e76ffb71ef3be1caed4e20003bca67dd
saifur2233 <saifurnstuiit223344@gmail.com>
2022-09-02 08:48:47 +0600

    issue-4 if else condition remove

--- src/main/java/com/example/storyapi/models/User.java
+++ src/main/java/com/example/storyapi/models/User.java
@@ -5,8 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Entity
 @Getter
@@ -18,15 +17,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String name;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     @Column(name="email", unique=true)
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
             flags = Pattern.Flag.CASE_INSENSITIVE)
     private String  email;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private String phoneNumber;
 
+    @NotNull
+    @NotBlank
+    @NotEmpty
     @Pattern(regexp ="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9@$!%*?&]{8,}")
     private String password;
 
