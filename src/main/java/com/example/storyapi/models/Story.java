0943691ce98833dafdc9e3ba82f6fe171acd9fd6
saifur2233 <saifurnstuiit223344@gmail.com>
2022-09-07 19:06:01 +0600

    issue-6 stories endpoints added

---
+++ src/main/java/com/example/storyapi/models/Story.java
@@ -0,0 +1,41 @@
+package com.example.storyapi.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Story {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty(message = "Blog title can't empty")
+    private String title;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty(message = "Blog description can't empty")
+    private String description;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty(message = "Blog author email can't empty")
+    private String authorEmail;
+
+    private LocalDateTime created_At = LocalDateTime.now();
+}
