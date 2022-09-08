771281a51004b8d17d221b9ce2db59220aea6b15
saifur2233 <saifurnstuiit223344@gmail.com>
2022-09-08 09:39:00 +0600

    issue-5 bcrypt password encoder added

--- src/main/java/com/example/storyapi/services/AuthService.java
+++ src/main/java/com/example/storyapi/services/AuthService.java
@@ -5,18 +5,27 @@ import com.example.storyapi.repositories.UserRepository;
 import com.example.storyapi.security.JWTUtility;
 import com.example.storyapi.utils.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class AuthService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private JWTUtility jwtUtility;
     public User signUp(User user){
-        return userRepository.save(user);
+        User newUser = new User();
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        newUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        newUser.setPhoneNumber(user.getPhoneNumber());
+        return userRepository.save(newUser);
     }
 
     public Optional<User> signIn(User user){
